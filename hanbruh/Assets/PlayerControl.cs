c127a09aa77e84aac07fc1b4e1c2df459917909a
SONSUZLUKxd <107800534+SONSUZLUKxd@users.noreply.github.com>
2022-07-08 18:05:48 +0300

    mert ehhe

---
+++ hanbruh/Assets/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 velocity;
+
+
+    public Transform ground;
+    public float distance = 0.3f;
+
+
+    public float speed;
+    public float jumpHeigh;
+    public float gravity;
+
+    public LayerMask mask;
+    private bool isGrounded;
+    private static readonly CharacterController velosj;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+}
